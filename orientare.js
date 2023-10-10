926372baa13ce7e3307482c756f2bed9e72e254e
dariusv2307 <115258057+dariusv2307@users.noreply.github.com>
2023-10-10 15:02:22 +0300

    Update orientare.js

--- orientare.js
+++ orientare.js
@@ -1,23 +1,22 @@
-window.addEventListener("deviceorientation", 
-on_device_orientation);
-function on_device_orientation(evt)
-{
-var alpha = evt.alpha;
-var beta = evt.beta;
-var gamma = evt.gamma;
-document.getElementById("a").innerHTML = "alpha = "+alpha;
-document.getElementById("b").innerHTML = "beta = "+beta;
-document.getElementById("c").innerHTML = "gamma = "+gamma;
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
-var centru = {x:canvas.width/2, y:canvas.height/2}
-var latura_patrat = 50;
-context.setTransform(1, 0, 0, 1, 0, 0);
-context.clearRect(0,0,canvas.width, canvas.height);
-context.translate(centru.x/2, centru.y/2);
-context.rotate(alpha * Math.PI / 180);
-context.beginPath();
-context.rect(-latura_patrat/2,-latura_patrat/2, 
-latura_patrat, latura_patrat);
-context.stroke();
+window.addEventListener("deviceorientation", onDeviceOrientation);
+
+function onDeviceOrientation(evt) {
+    var gamma = evt.gamma;
+    document.getElementById("gammaValue").innerHTML = "Gamma = " + gamma;
+
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    var centerX = canvas.width / 2;
+    var centerY = canvas.height / 2;
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Calculați raza cercului în funcție de valoarea lui gamma
+    var radius = 50 + Math.abs(gamma) * 2; // Ajustați factorul de scalare aici
+
+    context.beginPath();
+    context.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+    context.fillStyle = "#6A0888"; // Schimbați culoarea după preferințe
+    context.fill();
+    context.stroke();
 }
